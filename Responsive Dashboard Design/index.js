5e07fd8445c436b9ba026806b3ad58cf52da5219
Aman Tiwary <114864835+amanbrand@users.noreply.github.com>
2024-11-25 00:20:14 +0530

    Responsive Dashboard Demo

---
+++ Responsive Dashboard Design/index.js
@@ -0,0 +1,39 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'block';
+    });
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-mode-variable');
+    darkMode.querySelector('i:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('i:nth-child(2)').classList.toggle('active');
+
+});
+
+
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+    <td>${order.productName}</td>
+    <td>${order.productNumber}</td>
+    <td>${order.paymentStatus}</td>
+    <td class="${order.status ==='Declined' ?
+    'primary' : order.status ==='Pending' ? 'warning': 'danger'}
+">${order.status}</td> 
+<td class="success">Details</td>
+
+`;
+tr.innerHTML = trContent;
+document.querySelector('table tbody').appendChild(tr);
+
+});
---
+++ Responsive Dashboard Design/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        prodctNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'},
+        
+        {
+            productName: 'CSS Full Course',
+            prodctNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'},
+
+        {
+             productName: 'Flex-Box Tutorial',
+            prodctNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'},
+       
+]
